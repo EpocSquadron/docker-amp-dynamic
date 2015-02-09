6db462d6773b50c7c16ee76fea3181461ffd0987
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2015-02-08 22:18:05 -0500

    Add more logging.

--- run-bare.sh
+++ run-bare.sh
@@ -2,10 +2,11 @@
 
 docker run \
 -v $HOME/Sites:/var/www \
+-v $HOME/Sites/.logs:/var/log \
 -v $HOME/Sites/.coreos-databases/mysql:/var/lib/mysql \
 -p 127.0.0.1:3306:3306 \
 -p 127.0.0.1:80:80 \
 -p 127.0.0.1:443:443 \
---name $1-dynamic \
+--name ramp-dynamic \
 -d \
-epocsquadron/$1-dynamic
+epocsquadron/ramp-dynamic
