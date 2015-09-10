897063f9647fbcbfa76781a456f379c8cec20ff5
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2015-09-10 11:04:18 -0400

    Add log volume to start script

--- run-bare.sh
+++ run-bare.sh
@@ -4,6 +4,7 @@ docker run \
 -v $HOME/Sites:/var/www \
 -v $HOME/Sites/.logs:/var/log \
 -v $HOME/Sites/.coreos-databases/mysql:/var/lib/mysql \
+-v $HOME/Sites/.logs:/var/log \
 -p 127.0.0.1:3306:3306 \
 -p 127.0.0.1:80:80 \
 -p 127.0.0.1:443:443 \
