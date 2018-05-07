cf7d73daea67000c5f48f2e808a0a7640ef1a1b4
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2018-05-07 16:08:57 -0400

    Use external volumes for mysql and logs so they persist

--- run-bare.sh
+++ run-bare.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 docker run \
--v $HOME/Sites:/var/www \
--p 127.0.0.1:3306:3306 \
--p 127.0.0.1:80:80 \
--p 127.0.0.1:443:443 \
---name ramp-dynamic \
--d \
-epocsquadron/ramp-dynamic:15.10
+    -v $HOME/Sites:/var/www \
+    -v ramp-database:/var/lib/mysql \
+    -v ramp-logs:/var/log \
+    -p 127.0.0.1:3306:3306 \
+    -p 127.0.0.1:80:80 \
+    -p 127.0.0.1:443:443 \
+    --name ramp-dynamic \
+    -d \
+    epocsquadron/ramp-dynamic:15.10
